e58f075e1c5d74d1bdb65372e453eda3e5f8a7c2
Eric Liu <eric.liu@uber.com>
2019-04-30 16:17:36 -0700

    Adds method to delete an implementation of an interface and add an interface.

--- plugins/src/main/java/com/ericliu/InterfaceMigrator.java
+++ plugins/src/main/java/com/ericliu/InterfaceMigrator.java
@@ -6,11 +6,16 @@ import com.intellij.notification.Notifications;
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.JavaPsiFacade;
 import com.intellij.psi.PsiClass;
+import com.intellij.psi.PsiClassType;
 import com.intellij.psi.PsiElementFactory;
+import com.intellij.psi.PsiJavaCodeReferenceElement;
 import com.intellij.psi.PsiReferenceList;
 import com.intellij.psi.search.GlobalSearchScope;
 import com.intellij.psi.search.searches.DirectClassInheritorsSearch;
 
+/**
+ * This migrator replaces Boat interface implementation with Ship interface.
+ */
 class InterfaceMigrator {
 
     private final Project project;
@@ -38,12 +43,36 @@ class InterfaceMigrator {
                 GlobalSearchScope.allScope(project)).findAll()) {
 
             final PsiReferenceList implementsList = boatImplementor.getImplementsList();
-            implementsList.add(psiElementFactory.createClassReferenceElement(shipClass));
+            removeBoatInterface(implementsList);
 
+            addShipInterface(boatImplementor, implementsList);
+        }
+    }
+
+    private void addShipInterface(final PsiClass boatImplementor, final PsiReferenceList implementsList) {
+        implementsList.add(psiElementFactory.createClassReferenceElement(shipClass));
+
+        Notifications.Bus.notify(new Notification(InterfaceMigrator.class.getName(),
+                "Found Implementor: " + boatImplementor.getName(),
+                "Adds interface " + shipClass.getName(),
+                NotificationType.INFORMATION));
+    }
+
+    private void removeBoatInterface(final PsiReferenceList implementsList) {
+        final PsiClassType boatType = psiElementFactory.createType(boatClass);
+
+        Notifications.Bus.notify(new Notification(InterfaceMigrator.class.getName(),
+                "Removing boat",
+                "Reference name: " + boatType.getClassName(),
+                NotificationType.INFORMATION));
+
+        for (final PsiJavaCodeReferenceElement referenceElement : implementsList.getReferenceElements()) {
             Notifications.Bus.notify(new Notification(InterfaceMigrator.class.getName(),
-                    "Found Implementor: " + boatImplementor.getName(),
-                    "Adds interface " + shipClass.getName(),
+                    "PsiJavaCodeReferenceElement",
+                    "Reference name: " + referenceElement.getQualifiedName(),
                     NotificationType.INFORMATION));
+
+            referenceElement.delete();
         }
     }
 
